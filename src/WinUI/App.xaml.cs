5291738d54ea4292b099c9994af54e505bc3891e
mojski <marcin.mojski@polskieradio.pl>
2023-12-20 12:30:39 +0100

    initial

---
+++ src/WinUI/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MvvmDialogs.DialogTypeLocators;
+
+namespace WinUI;
+
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MvvmDialogs;
+using System.Windows;
+using WinUI.ViewModels;
+
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        IServiceCollection services = new ServiceCollection();
+
+        services.AddSingleton<HelloViewModel>();
+
+        services.AddSingleton<IDialogTypeLocator, DialogTypeLocator>();
+        services.AddSingleton<IDialogService, DialogService>();
+
+        IServiceProvider provider = services.BuildServiceProvider();
+
+        Ioc.Default.ConfigureServices(provider);
+    }
+}
+
---
+++ src/WinUI/DialogTypeLocator.cs
@@ -0,0 +1,13 @@
+﻿using MvvmDialogs.DialogTypeLocators;
+using System.ComponentModel;
+
+namespace WinUI
+{
+  internal class DialogTypeLocator : IDialogTypeLocator
+  {
+    public Type Locate(INotifyPropertyChanged viewModel)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
