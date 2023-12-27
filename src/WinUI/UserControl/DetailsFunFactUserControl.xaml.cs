0b82c55b5ececb94724104b5828b32ec25dd1a25
mojski <marcin.mojski@polskieradio.pl>
2023-12-27 15:06:06 +0100

    details in same window 1

---
+++ src/WinUI/UserControl/DetailsFunFactUserControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WinUI.UserControl
+{
+    /// <summary>
+    /// Interaction logic for DetailsFunFactUserControl.xaml
+    /// </summary>
+    public partial class DetailsFunFactUserControl : System.Windows.Controls.UserControl
+    {
+        public DetailsFunFactUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
