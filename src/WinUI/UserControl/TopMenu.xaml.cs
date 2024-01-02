bcbc1f1c10b41e1b75f5f24087c0ac91380a2ffd
mojski <marcin.mojski@polskieradio.pl>
2024-01-02 21:39:48 +0100

    menu and toolbar moved to user controls

---
+++ src/WinUI/UserControl/TopMenu.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WinUI.UserControl
+{
+    /// <summary>
+    /// Interaction logic for TopMenu.xaml
+    /// </summary>
+    public partial class TopMenu : System.Windows.Controls.UserControl
+    {
+        public TopMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
